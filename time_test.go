180790eeac0841fa36baaa6f32cfbe7fb64094de
shynome <shynome@gmail.com>
2023-09-22 11:04:35 +0800

    fix test expect value

--- time_test.go
+++ time_test.go
@@ -16,5 +16,5 @@ func TestTimeParse(t *testing.T) {
 	var body = `{"time": "2023-09-22 10:13:29"}`
 	try.To(json.Unmarshal([]byte(body), &m))
 	timestamp := time.Time(m.Time).Unix()
-	assert.Equal(t, timestamp, int64(1695348809))
+	assert.Equal(t, int64(1695348809), timestamp)
 }
